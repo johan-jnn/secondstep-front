715e76f56bccf3cccaa9533aca41fa4230fe3f9f
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-07 16:59:27 +0200

    CartProductCard

--- app/components/CartProductCard.tsx
+++ app/components/CartProductCard.tsx
@@ -33,50 +33,42 @@ export default function CartLineItem({
   };
 
   return (
-    <li key={id} className="cart-line">
-      {image && (
-        <Image
-          alt={title}
-          aspectRatio="1/1"
-          data={image}
-          height={100}
-          loading="lazy"
-          width={100}
-        />
-      )}
-
-      <div>
-        <Link
-          prefetch="intent"
-          to={lineItemUrl}
-          onClick={() => {
-            if (layout === 'aside') {
-              // close the drawer
-              window.location.href = lineItemUrl;
-            }
-          }}
-        >
-          <CartLineQuantity line={line} />
-          <p>
-            <strong className="cart-line-title">{title}</strong>
-            <br />
-            <strong className="cart-line-subtitle">{subtitle}</strong>
-          </p>
-        </Link>
-        <div className="cart-line-size-price">
-          <ul>
-            {selectedOptions.map((option) => (
-              <li key={option.name}>
-                <p className="cart-line-size">
-                  {option.name}: {option.value}
-                </p>
-              </li>
-            ))}
-          </ul>
-          <CartLinePrice line={line} as="span" />
+    <div className="cart-product-card">
+      <CartLineQuantity line={line} />
+      <li key={id} className="cart-line">
+        {image && <img src={image.url} alt={title} />}
+        <div>
+          <Link
+            prefetch="intent"
+            to={lineItemUrl}
+            onClick={() => {
+              if (layout === 'aside') {
+                // close the drawer
+                window.location.href = lineItemUrl;
+              }
+            }}
+          >
+            <p>
+              <strong className="cart-line-title">{title}</strong>
+              <br />
+              <strong className="cart-line-subtitle">{subtitle}</strong>
+            </p>
+          </Link>
+          <div className="cart-line-size-price">
+            <ul>
+              {selectedOptions.map((option) => (
+                <li key={option.name}>
+                  <p className="cart-line-size">
+                    {option.name}: {option.value}
+                  </p>
+                </li>
+              ))}
+            </ul>
+            <CartLinePrice line={line} as="span" />
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </div>
   );
 }
 
