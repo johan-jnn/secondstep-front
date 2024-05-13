134738c441f29d70b8d16134e7aeb520c27ee9e7
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-13 17:17:38 +0200

    Featured Collection updates

--- app/components/HomePageEngagements.tsx
+++ app/components/HomePageEngagements.tsx
@@ -11,7 +11,7 @@ function HomePageEngagements() {
         <a href=" ">Découvrez le concept</a>
       </div>
       <div className="homepage-banner-rate">
-        <h2>Nous sommes notés 4.5/5 sur</h2>
+        <h2>Nous sommes notés 4.3/5 sur</h2>
         <img src="app/assets/Trustpilot.png" alt="google rating" />
       </div>
       <div className="homepage-banner-cardtext">
