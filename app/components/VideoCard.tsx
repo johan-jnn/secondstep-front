b6e3fdd7b57892becea233d73677ede8d213f828
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-14 16:00:06 +0200

    Ongoing changements

--- app/components/VideoCard.tsx
+++ app/components/VideoCard.tsx
@@ -2,18 +2,57 @@ import React from 'react';
 import './styles/videoCard.scss';
 import {Link} from 'react-router-dom';
 
-function VideoCard() {
+interface VideoCardProps {
+  backgroundcolor: string;
+  textColor: string;
+  num: string;
+  title: string;
+  text: string;
+  subtext: string;
+  boldKeywords: string[];
+}
+
+function VideoCard({
+  backgroundcolor,
+  text,
+  subtext,
+  title,
+  textColor,
+  num,
+  boldKeywords,
+}: VideoCardProps) {
+  function renderTextWithBoldKeywords(text: string) {
+    const parts = text.split(new RegExp(`(${boldKeywords.join('|')})`, 'i'));
+    return (
+      <p className="VideoCard-text">
+        {parts.map((part, index) => {
+          if (boldKeywords.includes(part)) {
+            return <strong key={index}>{part}</strong>;
+          } else {
+            return <span key={index}>{part}</span>;
+          }
+        })}
+      </p>
+    );
+  }
+
   return (
-    <div className="VideoCard">
-      <h1>01</h1>
+    <div
+      className="VideoCard"
+      style={{backgroundColor: backgroundcolor, color: textColor}}
+    >
+      <h1>{num}</h1>
       <Link to={' '}>
         <button className="VideoCard-btn">
           <img src=" " alt=" " />
         </button>
       </Link>
-      <p>
-        blablablablablablablablablablablablablablablablablablablablablablablablablablablabla
-      </p>
+      <div className="VideoCard-bottom">
+        <h1>{title}</h1>
+        {renderTextWithBoldKeywords(text)}
+        <br />
+        <p className="VideoCard-sub">{renderTextWithBoldKeywords(subtext)}</p>
+      </div>
     </div>
   );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -35,6 +35,39 @@ export default function Homepage() {
       </div>
       <HomePageEngagements />
       <RecommendedProducts products={data.recommendedProducts} />
+      <div className="video-cards">
+        <VideoCard
+          backgroundcolor="var(--color-light)"
+          textColor=" "
+          text=""
+          num="01"
+          title=""
+          subtext=" "
+          boldKeywords={[]}
+        />
+        <VideoCard
+          backgroundcolor="var(--color-dark)"
+          textColor="var(--color-light)"
+          num="02"
+          text="Chez SecondStep, l'autheticité des produits constitue un pilier fondamental de notre philosophie d'entreprise. Nous comprenons à quel point il est essentiel pour nos clients de se sentir en confiance lors de leurs achats."
+          title="Authentification"
+          subtext="C'est pourquoi nous avons instauré un processus de vérification rigoureux pour chaque article proposé sur notre site..."
+          boldKeywords={[
+            'SecondStep',
+            'confiance',
+            'processus de vérification rigoureux',
+          ]}
+        />
+        <VideoCard
+          backgroundcolor=" "
+          textColor=" "
+          text="blablablablablablablablabl ablablablablabla "
+          title="03"
+          boldKeywords={[]}
+          subtext=" "
+          num=" "
+        />
+      </div>
     </div>
   );
 }
