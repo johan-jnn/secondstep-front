9ca643038f41dcd3960a46609da286eab872bd32
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-17 12:27:54 +0200

    Faq Push

--- app/components/FAQ.tsx
+++ app/components/FAQ.tsx
@@ -2,16 +2,23 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './styles/faq.scss';
 import Icon from './Icon';
+import data from '../lib/constants/faqHomePage.json';
+import {
+  PlusIcon,
+  LogoInstagramIcon,
+  EmailIcon,
+  ChatIcon,
+  ChevronRightIcon,
+  PersonFilledIcon,
+} from '@shopify/polaris-icons';
 
-export interface FAQProps {
-  answers: {
-    question: string;
-    answer: string;
-    link?: string;
-  }[];
+interface FAQItemProps {
+  question: string;
+  answer: string[];
+  link?: string;
 }
 
-function FAQItem({question, answer, link}: FAQProps['answers'][number]) {
+function FAQItem({question, answer, link}: FAQItemProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleAnswer = (e: React.MouseEvent<HTMLAnchorElement>) => {
@@ -23,62 +30,76 @@ function FAQItem({question, answer, link}: FAQProps['answers'][number]) {
     <div className="faq-item">
       <div className="question">
         <p>{question}</p>
-        <Link to="" className="faq-link" onClick={toggleAnswer}>
-          <img src="app/assets/icons-plus.svg" alt="plus btn" />
+        <Link to={link || ''} className="faq-link" onClick={toggleAnswer}>
+          <Icon icon={PlusIcon} />
         </Link>
       </div>
-      {isOpen && <div className="answer">{answer}</div>}
+      {isOpen && (
+        <div className="answer">
+          {answer.map((paragraph, index) => (
+            <React.Fragment key={index}>
+              <p>{paragraph}</p>
+              {index !== answer.length - 1 && <br />}{' '}
+              {/* Ajout de <br /> si ce n'est pas le dernier paragraphe */}
+            </React.Fragment>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 export default function FAQ() {
+  const faqList: FAQItemProps[] = data;
+
   return (
     <div className="faq">
       <div className="faq-socials">
         <div className="faq-socials-top">
           <p className="title">Une question?</p>
           <p>La team est là pour vous aider !</p>
-          <img src=" " alt=" " />
         </div>
-        <div className="faq-socials-min">
-          <img src=" " alt=" " />
+        <a href="mailto:contact@second-step.fr" className="faq-socials-min">
+          <Icon icon={EmailIcon} customStyling={{fill: 'var(--color-grey)'}} />
           <p>Mail : contact@second-step.fr</p>
-          <img src=" " alt=" " />
-        </div>
-        <div className="faq-socials-min">
-          <img src=" " alt=" " />
+          <Icon
+            icon={ChevronRightIcon}
+            customStyling={{fill: 'var(--color-grey)'}}
+          />
+        </a>
+        <a href="whatsapp://send?phone=0785838528" className="faq-socials-min">
+          <Icon icon={ChatIcon} customStyling={{fill: 'var(--color-grey)'}} />
           <p>Chat WhatsApp : 07 85 83 85 28</p>
-          <img src=" " alt=" " />
-        </div>
-        <div className="faq-socials-min">
-          <img src=" " alt=" " />
+          <Icon
+            icon={ChevronRightIcon}
+            customStyling={{fill: 'var(--color-grey)'}}
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/secondstep.fr/"
+          className="faq-socials-min"
+        >
+          <Icon
+            icon={LogoInstagramIcon}
+            customStyling={{fill: 'var(--color-grey)'}}
+          />
           <p>Instagram : @secondstep.fr</p>
-          <img src=" " alt=" " />
-        </div>
+          <Icon
+            icon={ChevronRightIcon}
+            customStyling={{fill: 'var(--color-grey)'}}
+          />
+        </a>
       </div>
       <div className="faq-questions">
         <h1>Question Fréquentes</h1>
-        <FAQItem
-          question="Qu'est-ce que React?"
-          answer="React est une bibliothèque JavaScript pour construire des interfaces utilisateur." // Réponse en tant que chaîne de caractères normale
-        />
-        <FAQItem
-          question="Quelle est la différence entre React et Angular?"
-          answer="React est une bibliothèque JavaScript pour la construction d'interfaces utilisateur, tandis qu'Angular est un framework JavaScript complet." // Réponse en tant que chaîne de caractères normale
-        />
-        <FAQItem
-          question="Quelle est la différence entre React et Angular?"
-          answer="React est une bibliothèque JavaScript pour la construction d'interfaces utilisateur, tandis qu'Angular est un framework JavaScript complet." // Réponse en tant que chaîne de caractères normale
-        />
-        <FAQItem
-          question="Quelle est la différence entre React et Angular?"
-          answer="React est une bibliothèque JavaScript pour la construction d'interfaces utilisateur, tandis qu'Angular est un framework JavaScript complet." // Réponse en tant que chaîne de caractères normale
-        />
-        <FAQItem
-          question="Quelle est la différence entre React et Angular?"
-          answer="React est une bibliothèque JavaScript pour la construction d'interfaces utilisateur, tandis qu'Angular est un framework JavaScript complet." // Réponse en tant que chaîne de caractères normale
-        />
+        {faqList.map((faq, index) => (
+          <FAQItem
+            key={index}
+            question={faq.question}
+            answer={faq.answer}
+            link={faq.link}
+          />
+        ))}
       </div>
     </div>
   );
