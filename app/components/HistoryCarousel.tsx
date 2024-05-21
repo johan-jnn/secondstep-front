15f30ab080c5fcf47ec1d72eaaac449296de99a4
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-21 14:55:09 +0200

    responsive push

--- app/components/HistoryCarousel.tsx
+++ app/components/HistoryCarousel.tsx
@@ -9,7 +9,6 @@ import './styles/historyCarousel.scss';
 
 // Importez votre JSON
 import data from '../lib/constants/historyCarousel.json';
-
 export default function HistoryCarousel() {
   return (
     <div className="history-carousel">
@@ -17,11 +16,37 @@ export default function HistoryCarousel() {
       <div className="history-carousel-sub">
         <Swiper
           modules={[Virtual, Navigation]}
-          slidesPerView={4}
-          centeredSlides={false}
-          spaceBetween={30}
+          slidesPerView={3}
+          centeredSlides={true}
+          spaceBetween={100}
           navigation={true}
-          virtual
+          breakpoints={{
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            // when window width is >= 768px
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+            // when window width is >= 1024px
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+            // when window width is >= 1200px
+            1200: {
+              slidesPerView: 3,
+              spaceBetween: 100,
+            },
+          }}
         >
           {data.History.map((item, index) => (
             <SwiperSlide key={index}>
