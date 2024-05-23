04bc51a3e9bbe8e1cbe09315c680d698b47cc81a
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-23 18:58:57 +0200

    Page Décourvrir

---
+++ app/routes/nous-decouvrir.tsx
@@ -0,0 +1,64 @@
+import '../styles/pages.scss';
+import image1 from 'app/assets/history-img.png';
+import image2 from 'app/assets/Nousdecimg2.png';
+import image3 from 'app/assets/equipe-full.png';
+import { Link } from 'react-router-dom';
+export default function NousDecouvrirPage() {
+  return (
+    <div className="nous-dec">
+      <div className="nous-dec-first">
+        <div className="nous-dec-first-text">
+          <h4>Notre Vision</h4>
+          <h2>Donner une nouvelle vie aux sneakers</h2>
+          <p>
+            Depuis nos modestes débuts en tant que passionnés de sneakers, nous
+            avons toujours été fascinés par la richesse de l’histoire que chaque
+            paire de chaussures portait. C’est cette fascination qui nous a
+            conduit à créer notre boutique de sneakers seconde main, avec pour
+            objectif principal de redonner une nouvelle vie à ces sneakers
+            pleines de caractère.
+            <br />
+            <br />
+            Chaque paire que nous acquérons raconte une histoire unique, qu’il
+            s’agisse d’une édition limitée qui a marqué une époque ou d’un
+            modèle classique qui a accompagné des générations de sneakerheads.
+          </p>
+        </div>
+        <img src={image1} alt=" " />
+      </div>
+      <div className="nous-dec-sec">
+        <div className="nous-dec-sec-text">
+          <h4>Notre Vision</h4>
+          <h2>Donner une nouvelle vie aux sneakers</h2>
+          <p>
+            Depuis nos modestes débuts en tant que passionnés de sneakers, nous
+            avons toujours été fascinés par la richesse de l’histoire que chaque
+            paire de chaussures portait. C’est cette fascination qui nous a
+            conduit à créer notre boutique de sneakers seconde main, avec pour
+            objectif principal de redonner une nouvelle vie à ces sneakers
+            pleines de caractère.
+            <br />
+            <br />
+            Chaque paire que nous acquérons raconte une histoire unique, qu’il
+            s’agisse d’une édition limitée qui a marqué une époque ou d’un
+            modèle classique qui a accompagné des générations de sneakerheads.
+          </p>
+        </div>
+        <img src={image2} alt=" " />
+      </div>
+      <p>
+        « Lorsque les gens achètent des chaussures chez Second Step, ils
+        n’achètent pas seulement des baskets ; ils adhèrent à une histoire, à un
+        mouvement, à un changement. »
+      </p>
+      <div className="nous-dec-last">
+        <p>Notre mission</p>
+        <h2>Redéfinir la consommation de sneakers</h2>
+        <img src={image3} alt=" " />
+      </div>
+      <Link to="/contact">
+        <button>Nous rejoindre &nbsp; &#x2794;</button>
+      </Link>
+    </div>
+  );
+}
--- app/routes/pages.$handle.tsx
+++ app/routes/pages.$handle.tsx
@@ -1,16 +1,8 @@
 import {json, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 import {useLoaderData, type MetaFunction} from '@remix-run/react';
-import '~/styles/pages.scss';
+import ContactPage from './contact';
 import FAQ from '~/components/FAQ';
-import Icon from '~/components/Icon';
-import {
-  PlusIcon,
-  LogoInstagramIcon,
-  EmailIcon,
-  ChatIcon,
-  ChevronRightIcon,
-  PersonFilledIcon,
-} from '@shopify/polaris-icons';
+import NousDecouvrirPage from './nous-decouvrir';
 
 export const meta: MetaFunction<typeof loader> = ({data}) => {
   return [{title: `Hydrogen | ${data?.page.title ?? ''}`}];
@@ -44,76 +36,13 @@ export default function Page() {
         <>
           <ContactPage />
         </>
-      ) : (
-        <div>Page Not Found</div>
-      )}
+      ) : null}
+      {page.handle === 'nous-d-couvrir' ? (
+        <>
+          <NousDecouvrirPage />
+        </>
+      ) : null}
       <FAQ />
-    </div>
-  );
-}
-
-export function ContactPage() {
-  return (
-    <div className="contact-component">
-      <div className="contact-card">
-        <a href="mailto:contact@second-step.fr" className="top">
-          <Icon
-            icon={EmailIcon}
-            customStyling={{fill: 'var(--color-primary)'}}
-          />
-          <h2>Nous Contacter par mail</h2>
-          <Icon
-            icon={ChevronRightIcon}
-            customStyling={{fill: 'var(--color-primary)'}}
-          />
-        </a>
-        <p>
-          N’hésitez pas à remplir notre formulaire de contact avec tous les
-          détails de votre demande.
-          <br />
-          <br />
-          Promis, on vous répondra dans les 24 à 48 heures ouvrables. 😊
-        </p>
-      </div>
-      <div className="contact-card">
-        <a href="whatsapp://send?phone=0785838528" className="top">
-          <Icon
-            icon={ChatIcon}
-            customStyling={{fill: 'var(--color-primary)'}}
-          />
-          <h2>Discuter sur Whatsapp</h2>
-          <Icon
-            icon={ChevronRightIcon}
-            customStyling={{fill: 'var(--color-primary)'}}
-          />
-        </a>
-        <p>
-          Discutez directement avec notre équipe via le chat ou sur WhatsApp au
-          +33 7 85 83 85 28.
-          <br />
-          <br />
-          Nous sommes disponibles du lundi au vendredi, de 09h00 à 19h00 GMT+1.
-        </p>
-      </div>
-      <div className="contact-card">
-        <a href="https://www.instagram.com/secondstep.fr/" className="top">
-          <Icon
-            icon={LogoInstagramIcon}
-            customStyling={{fill: 'var(--color-primary)'}}
-          />
-          <h2>Slider dans nos DM</h2>
-          <Icon
-            icon={ChevronRightIcon}
-            customStyling={{fill: 'var(--color-primary)'}}
-          />
-        </a>
-        <p>
-          Contactez-nous en DM Instagram sur @seconstep.fr !
-          <br />
-          <br />
-          Nous sommes disponibles du lundi au vendredi, de 09h00 à 19h00 GMT+1.
-        </p>
-      </div>
     </div>
   );
 }
