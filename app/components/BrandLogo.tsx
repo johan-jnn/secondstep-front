5f64143d39faffca30fb0551a1c5b00fc1497d5f
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-24 14:04:11 +0200

    push

--- app/components/BrandLogo.tsx
+++
@@ -1,9 +0,0 @@
-export type ValidBrands = 'Nike' | 'New Balance' | 'adidas' | 'Air Jordan';
-
-export interface BrandLogoProps {
-  brand: ValidBrands;
-}
-export default function BrandLogo(props: BrandLogoProps) {
-  const logo = props.brand.toLowerCase().replaceAll(' ', '_');
-  return <img src={`/brands_icons/${logo}.svg?url`} alt={`Logo of ${logo}`} />;
-}
