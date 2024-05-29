313aed6c5954ec215601f6ac348c7d6000896276
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-29 16:28:51 +0200

    Modif Homepages

--- app/components/Engagements.tsx
+++ app/components/Engagements.tsx
@@ -6,18 +6,20 @@ function Engagements() {
   return (
     <div className="banner-informations">
       <div className="banner-engagements">
-        <h1>Nos Engagements</h1>
+        <h2>Nos Engagements</h2>
         <a href=" ">Découvrez le concept</a>
       </div>
-      <div className="banner-rate">
-        <h3>Nous sommes notés 4.3/5 sur</h3>
-        <img src="app/assets/Trustpilot.png" alt="google rating" />
-      </div>
-      <div className="engagements-cards">
-        <CarteAuthenticite />
-      </div>
-      <div className="engagements-cards">
-        <Reconditionnement />
+      <div className="engagements-right">
+        <div className="banner-rate">
+          <h3>Nous sommes notés 4.3/5 sur</h3>
+          <img src="app/assets/Trustpilot.png" alt="google rating" />
+        </div>
+        <div className="engagements-cards">
+          <CarteAuthenticite />
+        </div>
+        <div className="engagements-cards">
+          <Reconditionnement />
+        </div>
       </div>
     </div>
   );
