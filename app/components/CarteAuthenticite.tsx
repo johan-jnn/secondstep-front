40c0f88e2403acb3cab565e9e1ccb3e70cd5769c
louis.prigneaux <louis.prigneaux@epita.fr>
2024-05-30 16:56:34 +0200

    Changements homepage

--- app/components/CarteAuthenticite.tsx
+++ app/components/CarteAuthenticite.tsx
@@ -3,20 +3,25 @@ import './styles/carteAuthenticite.scss';
 
 export default function CarteAuthenticite() {
   return (
-    <div className="carteAuthenticite">
-      <div className="content">
-        <h3>Premier concept seconde main en France</h3>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere,
-          voluptate iure. Sed, fugiat explicabo distinctio rem maxime nostrum
-          omnis nemo quae ea vel, nam enim quia veritatis laborum possimus
-          delectus.
-        </p>
-        <a href="/carte-authenticite">
-          <button>Découvrir le concept</button>
-        </a>
+    <div className="carteAutheticite-bg">
+      <div className="carteAuthenticite">
+        <div className="content">
+          <h3>Premier concept seconde main en France</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere,
+            voluptate iure. Sed, fugiat explicabo distinctio rem maxime nostrum
+            omnis nemo quae ea vel, nam enim quia veritatis laborum possimus
+            delectus.
+          </p>
+          <a href="/carte-authenticite">
+            <button>Découvrir le concept</button>
+          </a>
+        </div>
+        <img
+          src={CarteAuthenticiteIMG}
+          alt="Carte d'authenticité Second Step"
+        />
       </div>
-      <img src={CarteAuthenticiteIMG} alt="Carte d'authenticité Second Step" />
     </div>
   );
 }
